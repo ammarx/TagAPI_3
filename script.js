ab9fcc1da62cf1589e260748b8a5ad08c0e6d182
Ammar Ahmad <ammar.ahmad1993@gmail.com>
2016-06-02 19:04:43 +0500

    add Natives Path js

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ var getJsonLibrariesDownloadsClassifiersNativesX = function(name, natives_OS) {
 	}
 	return(p);   
 };
+
+
+var getJsonLibrariesDownloadsClassifiersNativesY = function(name, natives_OS) {
+	var data = name;
+	var json = JSON.parse(data);	
+
+	var p = "";
+	var n = 0;
+	for (i = 0; i < 500; i++){	
+		try{
+			p = p + json.libraries[n].downloads.classifiers[natives_OS].path + "\n";
+			n = n + 1;
+		} catch (err) {
+			n = n + 1;
+		}    
+	}
+	return(p);   
+};
