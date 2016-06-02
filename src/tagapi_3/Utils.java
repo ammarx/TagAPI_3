0d21d47ecd43c56f7ae95692676d50d9f9d751a3
Ammar Ahmad <ammar.ahmad1993@gmail.com>
2016-06-02 22:21:52 +0500

    add jarExtract

--- src/tagapi_3/Utils.java
+++ src/tagapi_3/Utils.java
@@ -5,6 +5,7 @@
  */
 package tagapi_3;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.MessageDigest;
@@ -140,4 +141,37 @@ public class Utils {
         }
 
     }
+
+    public void jarExtract(String OS, String _jarFile, String destDir) {
+        try {
+            Utils utils = new Utils();
+            _jarFile = utils.setMineCraft_Versions_X_NativesLocation(OS, _jarFile);
+            //_jarFile = _jarFile.replace("https://libraries.minecraft.net", "/home/ammar/NetBeansProjects/TagAPI_3/testx/libraries");
+            File dir = new File(destDir);
+            dir.mkdirs();
+
+            File jarFile = new File(_jarFile);
+
+            java.util.jar.JarFile jar = new java.util.jar.JarFile(jarFile);
+            java.util.Enumeration enumEntries = jar.entries();
+            while (enumEntries.hasMoreElements()) {
+                java.util.jar.JarEntry file = (java.util.jar.JarEntry) enumEntries.nextElement();
+                java.io.File f = new java.io.File(destDir + java.io.File.separator + file.getName());
+                if (file.isDirectory()) { // if its a directory, create it
+                    f.mkdirs();
+                    continue;
+                }
+                java.io.InputStream is = jar.getInputStream(file); // get the input stream
+                java.io.FileOutputStream fos = new java.io.FileOutputStream(f);
+                while (is.available() > 0) {  // write contents of 'is' to 'fos'
+                    fos.write(is.read());
+                }
+                fos.close();
+                is.close();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
 }
