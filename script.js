212d75a9952d552a6c5e0dc3add6db9bbc462ebf
Ammar Ahmad <ammar.ahmad1993@gmail.com>
2016-06-16 08:29:27 +0500

    add natives name

--- script.js
+++ script.js
@@ -32,3 +32,25 @@ var getJsonLibrariesDownloadsClassifiersNativesY = function(name, natives_OS) {
 	}
 	return(p);   
 };
+
+
+var getJsonLibrariesDownloadsClassifiersNativesZ = function(name) {
+	var data = name;
+	var json = JSON.parse(data);	
+
+	var p = ""; //this is for name
+	var n = 0;
+	for (i = 0; i < 500; i++){	
+		try{
+			if(json.libraries[n]['natives']){
+				p = p + json.libraries[n]['name'] + "\n";
+				n = n + 1;
+			} else {
+				n = n + 1;
+			}
+		} catch (err) {
+			n = n + 1;
+		}    
+	}
+	return(p);   
+};
