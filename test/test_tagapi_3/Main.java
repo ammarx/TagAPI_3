6602df3cb0d3e8fa72772115cf0e670c0fe10052
Ammar Ahmad <ammar.ahmad1993@gmail.com>
2016-08-18 05:36:05 +0500

    remove unused code

--- test/test_tagapi_3/Main.java
+++ test/test_tagapi_3/Main.java
@@ -4,10 +4,6 @@
  * and open the template in the editor.
  */
 package test_tagapi_3;
-
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  *
  * @author ammar
@@ -24,26 +20,15 @@ public class Main {
             
         }
         
-        /*
-        System.out.println("\n\nReading json for versions installed:");
-        for(Object installedVersionsList : API.getProfileInstalledVersionsList()) {
-            //read json for versions inalled.
-            System.out.println(installedVersionsList);
-        
-        }
-        */
-        
         System.out.println("\n\nSynicing file system with json...");
         API.syncVersions();
         
         String UsernameToUse = "Ammar_Ahmad";
-        String VersionToUse = "1.7.2";
+        String VersionToUse = "1.8.9";
         String MemoryToUse = "2G";
         API.setMemory(MemoryToUse);
         API.runMinecraft(UsernameToUse, VersionToUse);
        
-        
-        //so the downloads is fine... that means running has issies..
         //API.downloadVersionManifest();
         //API.downloadMinecraft("1.10.2-forge1.10.2-12.18.1.2011");
     }
