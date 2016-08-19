b503c81ca4524ebf9ec64606da9bdae32fecfd2f
Ammar Ahmad <ammar.ahmad1993@gmail.com>
2016-08-19 06:15:39 +0500

    implement installableVersionsList

--- test/test_tagapi_3/Main.java
+++ test/test_tagapi_3/Main.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package test_tagapi_3;
+
 /**
  *
  * @author ammar
@@ -12,25 +13,28 @@ public class Main {
 
     public static void main(String[] args) {
         tagapi_3.API_Interface API = new tagapi_3.API_Interface();
-        
+
         System.out.println("Reading file system for versions installed:");
         for (Object installedVersionsList : API.getInstalledVersionsList()) {
             //read file system for versions installed.
             System.out.println(installedVersionsList);
-            
+
         }
-        
+        /*
+        for (Object installableVersionsList : API.getInstallableVersionsList()) {
+            System.out.println(installableVersionsList); // can be split using " % "
+        }
+        */
         System.out.println("\n\nSynicing file system with json...");
         API.syncVersions();
-        
+
         String UsernameToUse = "Ammar_Ahmad";
-        String VersionToUse = "1.8.9";
+        String VersionToUse = "1.10.2-OptiFine_HD_U_C2";
         String MemoryToUse = "2G";
         API.setMemory(MemoryToUse);
         API.downloadVersionManifest();
         API.downloadMinecraft(VersionToUse, false); //force download flag
         API.runMinecraft(UsernameToUse, VersionToUse);
-       
 
     }
 
