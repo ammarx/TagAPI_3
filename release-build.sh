cf190f45750242991243d2eb6706a31553fa1ce6
Ammar Ahmad <ammar.ahmad1993@gmail.com>
2016-08-30 14:23:11 +0500

    add release script

---
+++ release-build.sh
@@ -0,0 +1,17 @@
+cd dist/
+cd lib/
+jar xf commons-io-2.5.jar
+jar xf json-20160212.jar
+jar xf json-simple-1.1.1.jar
+cd ../lib/; jar xf ../TagAPI_3.jar
+rm commons-io-2.5.jar
+rm json-20160212.jar
+rm json-simple-1.1.1.jar
+cd ../
+rm TagAPI_3.jar
+cd lib/
+zip TagAPI_3.zip * -r
+mv TagAPI_3.zip ../TagAPI_3.jar
+pwd
+cd ../
+rm -rf lib
