faaee0d47bbf11c75c477fff5a15b2387c1ee05b
Ammar Ahmad <ammar.ahmad1993@gmail.com>
2017-02-11 02:47:23 +0500

    modify version number

--- src/tagapi_3/API_Interface.java
+++ src/tagapi_3/API_Interface.java
@@ -18,7 +18,7 @@ import java.util.stream.Stream;
 public class API_Interface {
 
     public String getAPIVersion() {
-        return "v0.6-alpha";
+        return "v0.7-alpha";
     }
 
     public String getUpdateStatus() {
