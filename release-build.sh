44f1fdaeefcb029338181136093dd436155421da
Ammar Ahmad <ammar.ahmad1993@gmail.com>
2017-02-13 01:22:57 +0500

    modify release-build.sh

--- release-build.sh
+++ release-build.sh
@@ -4,10 +4,14 @@ cd lib/
 jar xf commons-io-2.5.jar
 jar xf json-20160212.jar
 jar xf json-simple-1.1.1.jar
+jar xf HEX_MOD.jar
+jar xf zt-zip-1.11.jar
 cd ../lib/; jar xf ../TagAPI_3.jar
 rm commons-io-2.5.jar
 rm json-20160212.jar
 rm json-simple-1.1.1.jar
+rm HEX_MOD.jar
+rm zt-zip-1.11.jar
 cd ../
 rm TagAPI_3.jar
 cd lib/
