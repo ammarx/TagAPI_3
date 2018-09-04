8e33eb0ad878631192f5ee9975661fff4b0fbaf4
Unknown <ammar.ahmad1993@gmail.com>
2018-09-04 23:55:54 +0500

    add version updates

--- src/tagapi_3/API_Interface.java
+++ src/tagapi_3/API_Interface.java
@@ -44,7 +44,7 @@ import java.util.stream.Stream;
 public class API_Interface {
 
     public String getAPIVersion() {
-        return "v0.10-alpha";
+        return "v0.11-alpha";
     }
 
     public String getUpdateStatus() {
